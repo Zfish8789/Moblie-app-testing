9abb7d7d8be62974fdb478230c99b657752df3a6
Zfish8789 <90352232+Zfish8789@users.noreply.github.com>
2023-01-30 15:15:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  document.getElementById("Active User").innerHTML = "Johnathan Douglas";
+})
+
+function expandMenu() {
+  var popouts = document.getElementsByClassName("Popout")
+  for (var i = 0; i < popouts.length; i++) {
+    popouts.item(i).classList.add("show")
+  }
+}
+
+function expandItem(className) {
+  console.log(className.innerHTML)
+  var item = document.getElementsByClassName(className.innerHTML)
+  for (var i = 0; i < item.length; i++) {
+    item.item(i).classList.add("expand")
+  }
+}
+
+document.onclick = function(event) {
+  var target = 'target' in event ? event.target : event.srcElement;
+  if (!target.classList.contains("menuKept")) {
+    var popouts = document.getElementsByClassName("Popout")
+    for (var i = 0; i < popouts.length; i++) {
+      popouts.item(i).classList.remove("show")
+    }
+  }
+  if (!target.classList.contains("ItemKeepOne")) {
+    var popouts = document.getElementsByClassName("MainItemOne")
+    for (var i = 0; i < popouts.length; i++) {
+      popouts.item(i).classList.remove("expand")
+    }
+  }
+  if (!target.classList.contains("ItemKeepTwo")) {
+    var popouts = document.getElementsByClassName("MainItemTwo")
+    for (var i = 0; i < popouts.length; i++) {
+      popouts.item(i).classList.remove("expand")
+    }
+  }
+};
